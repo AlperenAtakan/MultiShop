ece1d7883e95ecfe0532b3ab9153015f7fd851c9
Alperen <AlperenAtakan3883@gmail.com>
2024-09-26 19:15:15 +0300

    Add project files.

---
+++ Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class ProductListController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ProductListController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public IActionResult Index(string id)
+        {
+            ViewBag.directory1 = "Ana Sayfa";
+            ViewBag.directory2 = "Ürünler";
+            ViewBag.directory3 = "Ürün Listesi";
+            ViewBag.i = id;
+            return View();
+        }
+
+        public IActionResult ProductDetail(string id)
+        {
+            ViewBag.directory1 = "Ana Sayfa";
+            ViewBag.directory2 = "Ürün Listesi";
+            ViewBag.directory3 = "Ürün Detayları";
+            ViewBag.x = id;
+            return View();
+        }
+
+
+       
+    }
+}
